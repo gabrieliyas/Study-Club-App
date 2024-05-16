8120785aeb8b1b6ee6dcafcdc82a264019e485b9
Gabriel Iyas <giyas9970@gmail.com>
2024-05-17 01:01:45 +0700

    Tidy up folders and files

--- index.js
+++ index.js
@@ -15,22 +15,9 @@ app.use(express.json());
 
 db.connect ((err) => {
     if (err) throw err;
-    console.log("database connected");
+    // console.log("database connected");
 })
 
+app.use("/", require("./routes/pages"));
+
 app.listen(PORT);
-
-//loginscript.js
-/*
-const container = document.getElementById("container");
-const registerBtn = document.getElementById("register");
-const loginBtn = document.getElementById("login");
-
-registerBtn.addEventListener("click", () => {
-  container.classList.add("active");
-});
-
-loginBtn.addEventListener("click", () => {
-  container.classList.remove("active");
-});
-*/
