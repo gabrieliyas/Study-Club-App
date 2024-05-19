5659856fc252af10b94b9276cbf6ab3674c62105
Gabriel Iyas <giyas9970@gmail.com>
2024-05-19 12:24:52 +0700

    Regular update

--- public/js/loginscript.js
+++ public/js/loginscript.js
@@ -19,11 +19,41 @@ registerBtn.addEventListener("click", () => {
   }).then(res => res.json())
   .then(data => {
     if (data.status == "error") {
+      success.style.display = "none"
+      error.style.display = "block"
+      error.innerText = data.error
+    } else {
+      error.style.display = "none"
       success.style.display = "block"
+      error.InnerText = data.success
     }
   })
 });
 
 loginBtn.addEventListener("click", () => {
   container.classList.remove("active");
+
+  const login = {
+    email: email.value,
+    password: password.value
+  }
+
+  fetch("/api/login", {
+    metdho: "POST",
+    body: JSON.stringify(register),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => res.json())
+  .then(data => {
+    if (data.status == "error") {
+      success.style.display = "none"
+      error.style.display = "block"
+      error.innerText = data.error
+    } else {
+      error.style.display = "none"
+      success.style.display = "block"
+      error.InnerText = data.success
+    }
+  })
 });
